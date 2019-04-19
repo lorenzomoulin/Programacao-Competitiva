db57df844e70d905ea2c1386e8e7ea8f7ba5d5bd
lorenzo <lorenzomoulin@gmail.com>
2019-04-18 23:36:57 -0300

    201904182336

--- maratona/2017/primeira fase/C/macro.cpp
+++ maratona/2017/primeira fase/C/macro.cpp
@@ -1,29 +1,79 @@
 #include <bits/stdc++.h>
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define trav(a, x) for(auto &a: x)
+using namespace std; 
+  
+typedef long long int ll; 
+  
+// Utility function to find 
+// GCD of 'a' and 'b' 
+ll gcd(ll a, ll b) 
+{ 
+    if (b == 0) 
+        return a; 
+    return gcd(b, a % b); 
+} 
 
-using namespace std;
-
-int mdc (int a, int b)
-{
-	if (b == 0) return a;
-	return mdc (b, a%b);
+ll lcm(ll a, ll b){
+    return a*(b/gcd(a, b));
 }
 
-int mmc (int a, int b)
-{
+// Returns LCM of array elements 
+ll findlcm(ll arr[], ll n) 
+{ 
+    // Initialize result 
+    ll ans = arr[0]; 
+  
+    // ans contains LCM of arr[0], ..arr[i] 
+    // after i'th iteration, 
+    for (ll i = 1; i < n; i++) 
+        ans = (((arr[i] * ans)) / 
+                (gcd(arr[i], ans))); 
+  
+    return ans; 
+} 
 
-	return b*(a/mdc (b, a));
-}
+vector<ll> divisores;
+
+void divisors(ll n) 
+{ 
+    // Note that this loop runs till square root 
+    for (ll i=1; i * i <= n; i++) 
+    { 
+        if (n%i == 0) 
+        { 
+            // If divisors are equal, print only one 
+            if (n/i == i) 
+                divisores.pb(i);
+  
+            else // Otherwise print both 
+                 divisores.pb(i), divisores.pb(n / i);
+        } 
+    } 
+} 
+
+
 
 int main ()
 {
-	int N, L, i, m, r = 0;
-	scanf ("%d %d", &N, &L);
-	int n[N];
-	for (i = 0; i < N; i++) scanf ("%d", &(n[i]));
-	m = n[0];
-	for (i = 1; i < N; i++) m = mmc (m, n[i]);
-	r = (mdc (m, L/m))*(L/m);
-	if (L/mmc(m, r) != 1) r = r*(L/r);
-	printf ("%d\n", r);
+	ll N, L, i, m, r = 0;
+	scanf ("%lld %lld", &N, &L);
+	ll n[N];
+	for (i = 0; i < N; i++) scanf ("%lld", &(n[i]));
+	ll mmc = findlcm(n, N);
+    ll menor = L / mmc;
+    ll maior = menor*mmc;
+    divisors(maior);
+    ll res = 0;
+    sort(all(divisores));
+    trav(a, divisores){
+        if (lcm(a, mmc) == maior){
+            cout << a << "\n";
+            return 0;
+        }
+    }
+    cout << maior << endl;
+    
 	return 0;
 }
